59864f3dfb41b39003411dfc3bc5f3d929cfe9ab
Prachpaveen Y <prachpaveen888@gmail.com>
2023-01-25 08:29:57 +0700

    front-end

--- frontend-vuejs/add.js
+++ frontend-vuejs/add.js
@@ -1,7 +1,9 @@
-// var app {el: '#app'} เชื่อม <div id="app"> ที่ไฟล์index.html
+// var app {el} เชื่อม index.html
 var app = new Vue({
     el: '#app',
     data: {
       message: 'Build Assessment Websites with Vue.js'
     }
   })
+
+  //http-server : start
