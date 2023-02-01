f246595f62b30ba1110559e19258e0e5771bfd23
Prachpaveen Y <prachpaveen888@gmail.com>
2023-02-01 09:39:00 +0700

    update react

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Card, Col, Row, Icon, Collapse, Input } from 'antd';
+import Addform from '../src/components/addForm'
+import axios from 'axios'
+const { Panel } = Collapse;
+
